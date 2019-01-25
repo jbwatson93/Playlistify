c56768fbd74241d7d348e4334076b610cd8ee621
JB Watson <jbwatson93@gmail.com>
2019-01-25 12:31:46 -0500

    setting up react

---
+++ client/components/Playlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Playlist extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Playlist;
