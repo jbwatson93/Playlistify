77f2e27dbe305ca62059f803bbb282d1b2e89c3a
JB Watson <jbwatson93@gmail.com>
2019-01-27 20:05:07 -0500

    finished mvp

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -17,6 +17,7 @@ class Playlist extends Component {
         nowPlaying: 0,
         loaded: false,
         playing: false,
+        edited: false,
         newSong: {
             name: "",
             artist: '',
@@ -49,7 +50,7 @@ class Playlist extends Component {
             const newSong = {...this.state.playlists}
             
             newSong.songs.push(res.data)
-            this.setState({playlists: newSong})
+            this.setState({playlists: newSong,loaded: true })
         })
     }
     
@@ -58,14 +59,35 @@ class Playlist extends Component {
         .then((res) => {
           console.log(res.data)
           this.setState({ 
-              playlists: res.data,
-            loaded: true})
-        })
+              playlists: res.data})
+        }).then((res)=>{
+            console.log(this.state.playlists)
+            if(this.state.playlists.songs[0] === undefined){}
+            else{
+                this.setState({loaded: true})
+            }}
+        )
       }
       handleChange = (e) => {
         const newSong = { ...this.state.newSong }
         newSong[e.target.name] = e.target.value
         this.setState({ newSong })
+    }
+    handleChange2 = (e) => {
+        const playlists = { ...this.state.playlists }
+        playlists[e.target.name] = e.target.value
+        this.setState({ playlists })
+    }
+    deletePlaylist = () =>{
+        axios.delete(`/api/playlists/${this.props.match.params.id}`)
+    }
+    editing = () =>{
+        this.setState({edited: !this.state.edited})
+    }
+    updatePlaylist = (e) =>{
+        e.preventDefault()
+        axios.patch(`/api/playlists/${this.props.match.params.id}`, this.state.playlists)
+        .then(this.editing())
     }
     render() {
         const song =  this.state.playlists.songs[this.state.nowPlaying]
@@ -75,8 +97,10 @@ class Playlist extends Component {
                <Navbar/>
                <div className='page'>
                    <h1>{this.state.playlists.name}</h1>
+                   {this.state.edited? <div> <input onChange={(e) => this.handleChange2(e)} name='name' type='text'></input> <button className='addSong' onClick={this.updatePlaylist} > Update Name </button> </div> :null}
                    <p>{this.state.playlists.genre}</p>
-                   <button>Edit</button> 
+                   
+                   <button onClick={this.editing}>Edit</button> 
                  
             {this.state.loaded ? <div className='player-wrapper'><ReactPlayer className='react-player' 
              controls={true} url={song.url} 
@@ -97,6 +121,7 @@ class Playlist extends Component {
              <button className='addSong' onClick={this.addSong}>Add Song</button>
                </div>
                 </div>
+                <Link to='/playlists'> <button className='addSong' onClick={this.deletePlaylist}>Delete Playlist</button> </Link>
             </div>
         );   }
 }
